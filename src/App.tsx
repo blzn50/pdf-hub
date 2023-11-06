92d937c8695921e2fc89ea30e5d24076428a123b
Narendra Lama <blazeraze.naren@gmail.com>
2023-11-06 18:00:22 +0200

    lazy loaded Preview and Action components; moved file processing logic inside Action component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { lazy, Suspense, useCallback, useState } from 'react';
 
 import {
   DndContext,
@@ -10,12 +10,8 @@ import {
 } from '@dnd-kit/core';
 import { restrictToWindowEdges } from '@dnd-kit/modifiers';
 import { arrayMove } from '@dnd-kit/sortable';
-import { PDFDocument } from 'pdf-lib';
-import { pdfjs } from 'react-pdf';
 
-import { Action } from 'components/Action';
 import { Dropzone } from 'components/Dropzone';
-import { Preview } from 'components/Preview';
 
 import { objectId } from 'helpers/function';
 
@@ -23,16 +19,14 @@ import { CustomFile, DownloadLink } from 'types';
 
 import './App.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
-
 const measuringStrategy = {
   droppable: {
     strategy: MeasuringStrategy.Always,
   },
 };
+
+const Preview = lazy(() => import('./components/Preview'));
+const Action = lazy(() => import('./components/Action'));
 
 function App() {
   const [files, setFiles] = useState<CustomFile[]>([]);
@@ -78,76 +72,42 @@ function App() {
     setDownloadLink(undefined);
   };
 
-  const saveMergedPdf = useCallback(
-    (mergedFile: Uint8Array, filename: string) => {
-      const link = URL.createObjectURL(
-        new Blob([mergedFile], { type: 'application/pdf' }),
-      );
-      return { download: filename, href: link };
-    },
-    [],
-  );
-
-  const processPdfMerge = useCallback(async () => {
-    const mergePdf = await PDFDocument.create();
-
-    // convert files to arraybuffer
-    const arrayBufferList = files.map(
-      async (file) => await file.file.arrayBuffer(),
-    );
-
-    // copy all the pages and add to merge file
-    for (let index = 0; index < arrayBufferList.length; index++) {
-      const element = await arrayBufferList[index];
-      const pdfToMerge = await PDFDocument.load(element);
-
-      const copiedPages = await mergePdf.copyPages(
-        pdfToMerge,
-        pdfToMerge.getPageIndices(),
-      );
-
-      for (let index = 0; index < copiedPages.length; index++) {
-        const page = copiedPages[index];
-        mergePdf.addPage(page);
-      }
-    }
-
-    const mergedPdf = await mergePdf.save();
-    const saveInfo = saveMergedPdf(mergedPdf, 'merged-document.pdf');
-
-    setDownloadLink(saveInfo);
-  }, [files, saveMergedPdf]);
-
   return (
     <main className="pdf-merge">
       <h1 style={{ textAlign: 'center' }}>Merge PDF</h1>
-      {files.length === 0 && (
-        <Dropzone
-          onDrop={handleDragToDrop}
-          accept={{ 'application/pdf': ['.pdf'] }}
-        />
-      )}
-      {!!files.length && (
-        <>
-          <section className="files-manipulation__container">
-            <DndContext
-              onDragEnd={handleRearrangeFiles}
-              sensors={sensors}
-              modifiers={[restrictToWindowEdges]}
-              measuring={measuringStrategy}
-            >
-              <Preview files={files} onFileRemove={handleRemoveFile}>
-                <Dropzone
-                  withinFiles
-                  accept={{ 'application/pdf': ['.pdf'] }}
-                  onDrop={handleDragToDrop}
-                />
-              </Preview>
-            </DndContext>
-          </section>
-          <Action downloadLink={downloadLink} onClick={processPdfMerge} />
-        </>
-      )}
+      <Suspense fallback={<>Loading...</>}>
+        {files.length === 0 && (
+          <Dropzone
+            onDrop={handleDragToDrop}
+            accept={{ 'application/pdf': ['.pdf'] }}
+          />
+        )}
+        {!!files.length && (
+          <>
+            <section className="files-manipulation__container">
+              <DndContext
+                onDragEnd={handleRearrangeFiles}
+                sensors={sensors}
+                modifiers={[restrictToWindowEdges]}
+                measuring={measuringStrategy}
+              >
+                <Preview files={files} onFileRemove={handleRemoveFile}>
+                  <Dropzone
+                    withinFiles
+                    accept={{ 'application/pdf': ['.pdf'] }}
+                    onDrop={handleDragToDrop}
+                  />
+                </Preview>
+              </DndContext>
+            </section>
+            <Action
+              files={files}
+              downloadLink={downloadLink}
+              onSetDownloadLink={setDownloadLink}
+            />
+          </>
+        )}
+      </Suspense>
     </main>
   );
 }
