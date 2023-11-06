5d06c4818c1b37c58f7ee057d6bc5e7b6f76e266
Narendra Lama <blazeraze.naren@gmail.com>
2023-11-06 18:04:46 +0200

    vite config modified for splitting dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { dependencies } from './package.json';
+
 const require = createRequire(import.meta.url);
 const cMapsDir = path.join(
   path.dirname(require.resolve('pdfjs-dist/package.json')),
@@ -15,6 +17,15 @@ const standardFontsDir = path.join(
   path.dirname(require.resolve('pdfjs-dist/package.json')),
   'standard_fonts',
 );
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: Record<string, [string]> = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-dom'].indexOf(key) > -1) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,4 +39,14 @@ export default defineConfig({
       ],
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
