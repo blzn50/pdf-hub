ba5571c20d7cf1ad6340bab73b86b8001774e49f
Narendra Lama <blazeraze.naren@gmail.com>
2023-11-16 22:22:17 +0200

    fix the file method not available in test environment by installing blob-polyfill

--- setupTest.ts
+++ setupTest.ts
@@ -2,3 +2,12 @@
  * The solution was extracted from https://github.com/testing-library/jest-dom/issues/439.
  */
 import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+import { afterEach } from 'vitest';
+// To fix the missing methods of File in test
+// https://github.com/jsdom/jsdom/issues/2555
+import 'blob-polyfill';
+
+afterEach(() => {
+  cleanup();
+});
