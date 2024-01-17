dc74a2a3c6140623dc31caf89ecc585391f847fe
Narendra Lama <blazeraze.naren@gmail.com>
2024-01-17 14:54:36 +0200

    setup for test initialized; file testing also configured

--- setupTest.ts
+++ setupTest.ts
@@ -1,9 +1,9 @@
 /**
  * The solution was extracted from https://github.com/testing-library/jest-dom/issues/439.
  */
-import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 // To fix the missing methods of File in test
 // https://github.com/jsdom/jsdom/issues/2555
 import 'blob-polyfill';
@@ -11,3 +11,10 @@ import 'blob-polyfill';
 afterEach(() => {
   cleanup();
 });
+
+// https://github.com/jsdom/jsdom/issues/1721
+function noOp() {}
+
+if (typeof window.URL.createObjectURL === 'undefined') {
+  Object.defineProperty(window.URL, 'createObjectURL', { value: noOp });
+}
